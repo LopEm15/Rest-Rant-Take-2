da80c89c9005ffbc02a9e10172065606dc384ea1
Oscar Lopez <olop1597@gmail.com>
2023-02-06 11:44:46 -0800

    Establishing helper methods

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,24 +5,35 @@ function show (data) {
     return (
         <Def>
           <main className="show">
-            <h1>{data.place.name}</h1>
-           <div><img src={data.place.pic}/></div>
-            <h2>Rating</h2>
-            <p className="text-center">Not Rated</p>
-            <h2>Description</h2>
-            <p className="text-center">
-            Located in {data.place.city}, {data.place.state}
-            </p>
-            <h2>Comments</h2>
-            <p className="text-center">No comments yet!</p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                Edit
-            </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form>
+            <div className="row">
+                <h1>{data.place.name}</h1>
+            <div className="col-sm-6">
+                    <img src={data.place.pic}/>
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
+            </div>
+            <div className="col-sm-6">
+                <h2>Rating</h2>
+                <p className="text-center">Not Rated</p>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
+                <h2>Comments</h2>
+                <p className="text-center">No comments yet!</p>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+                </div>
+            </div>
           </main>
         </Def>
     )
