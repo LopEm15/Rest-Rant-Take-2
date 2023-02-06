67e4af08f3217b3bdd65e8201d627547bc8eaaed
Oscar Lopez <olop1597@gmail.com>
2023-02-06 14:26:01 -0800

    Adding custom validation error  messages

--- controllers/places.js
+++ controllers/places.js
@@ -14,13 +14,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     db.Place.create(req.body)
-        .then(() => {
-            res.redirect('/places')
-        })
-        .catch(err => {
-            console.log('err', err)
+    .then(() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        if (err && err.name == 'ValidationError') {
+            let message = 'Validation Error: '
+            for (var field in err.errors) {
+                message += `${field} was ${err.errors[field].value}. `
+                message += `${err.errors[field].message}`
+            }
+            console.log('Validation error message', message)
+            res.render('places/new', { message })
+        }
+        else {
             res.render('error404')
-        })
+        }
+    })
 })
 
 router.get('/new', (req, res) => {
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form () {
+function new_form (data) {
+    let message = ''
+    if (data.message){
+        message = (
+            <h4 className="alert-danger">
+                {data.message}
+            </h4>
+        )
+    }
     return (
         <Def>
           <main>
+            <div className="row">
             <h1>Add a New Place</h1>
+            {message}
             <form method="POST" action="/places">
                 <div className="form-group col-sm-4">
                     <label htmlFor="name">Place Name</label>
@@ -36,10 +46,9 @@ function new_form () {
                         name="founded" 
                         value={new Date().getFullYear()} />
                 </div>
-
-
                 <input className="btn btn-primary" type="submit" value="Add Place" />
             </form>
+            </div>
           </main>
         </Def>
     )
