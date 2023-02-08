da9bcd3450cf5f76820251434bee5ebc58cdde16
Oscar Lopez <olop1597@gmail.com>
2023-02-07 20:35:02 -0800

    Adding comments section

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map (c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main className="show">
@@ -31,8 +50,55 @@ function show (data) {
                     </button>
                 </form>
                 </div>
+                <div>
                 <h2>Comments</h2>
-                <p className="text-center">No comments yet!</p>
+                {comments}
+                </div>
+                <div className="comment form">
+                    <form method="POST" action="/show">
+                        <div className="row">
+                        <div className="col-sm-6 col-md-4 col-lg-3 form-group forms-name">
+                                <label htmlFor="author">Author:</label>
+                                <input className="form-control"
+                                placeholder="Your name here"
+                                type="text"
+                                id="author"
+                                name="author"/>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-3 form-group forms-name">
+                                <label htmlFor="content">Content</label>
+                                <input className="form-control"
+                                placeholder="Write your comment here!"
+                                type="textarea"
+                                id="content"
+                                name="content"/>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-3 form-group forms-name">
+                                <label htmlFor="stars" for="formControlRange"className="text-warning d-flex p-2">Star Rating</label>
+                                <input className="form-control-range"
+                                type="range"
+                                id="formControlRange"
+                                name="stars"/>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-3  d-flex p-2">
+                                <label htmlFor="rant" className="checkbox d-flex p-2">Rant 🤬 </label>
+                                    <input
+                                    className="checkbox d-flex p-2"
+                                    type="checkbox" 
+                                    id="rant" 
+                                    name="rant"
+                                    />
+                            </div>
+                        </div>
+                            <div className="p-2">
+                                <input 
+                                className=" btn-primary p-2" 
+                                type="submit" 
+                                value="Add Comment"
+                                />
+                            </div>
+                    </form>
+                </div>
             </div>
           </main>
         </Def>
